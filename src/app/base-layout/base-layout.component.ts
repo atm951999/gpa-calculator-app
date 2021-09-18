a3181669f63576eb50b5835064508ab7599af9e1
Angela Martin <amartin3@woh.rr.com>
2021-09-18 15:36:03 -0400

    first commit

---
+++ src/app/base-layout/base-layout.component.ts
@@ -0,0 +1,39 @@
+/*
+============================================
+; Title: Assignment 6.4
+; Author: Professor Krasso
+; Date: 12 September 2021
+; Modified By: Angela Martin
+; Description: This program demonstrates the
+; use of Angular.
+===========================================
+*/
+
+// Imports
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-base-layout',
+  templateUrl: './base-layout.component.html',
+  styleUrls: ['./base-layout.component.css']
+})
+export class BaseLayoutComponent implements OnInit {
+
+  assignment: string;
+
+  constructor(private cookieService: CookieService, private router: Router) {
+    this.assignment = 'GPA Calculator'
+  }
+
+  ngOnInit(): void {
+  }
+
+  signOut() {
+    this.cookieService.deleteAll();
+    this.router.navigate(['/session/sign-in']);
+  }
+
+}
